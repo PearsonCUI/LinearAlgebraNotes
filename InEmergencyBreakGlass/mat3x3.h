0fe4341bf132da14a1cdf817218f46c9c59d4e6d
Helen Ginn <github@hginn.co.uk>
2018-08-19 18:42:13 +0200

    Adding directory for emergency code supplies

---
+++ InEmergencyBreakGlass/mat3x3.h
@@ -0,0 +1,76 @@
+// Mandexing: a manual indexing program for crystallographic data.
+// Copyright (C) 2017-2018 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__mat3x3__
+#define __vagabond__mat3x3__
+
+#include <stdio.h>
+#include <vector>
+#include "vec3.h"
+#include <string.h>
+
+struct mat3x3
+{
+	double vals[9];
+};
+
+std::string mat3x3_desc(mat3x3 mat);
+
+mat3x3 mat3x3_inverse(mat3x3 &mat);
+mat3x3 mat3x3_from_unit_cell(double a, double b, double c, double alpha, double beta, double gamma);
+mat3x3 mat3x3_from_unit_cell(double *unitCell);
+
+void mat3x3_mult_scalar(mat3x3 *mat, double scale);
+void unit_cell_from_mat3x3(mat3x3 mat, double *vals);
+mat3x3 make_mat3x3();
+
+inline void mat3x3_mult_vec(struct mat3x3 mat, struct vec3 *vec)
+{
+	struct vec2 v;
+
+	v.x = mat.vals[0] * vec->x + mat.vals[1] * vec->y + mat.vals[2] * vec->z;
+	v.y = mat.vals[3] * vec->x + mat.vals[4] * vec->y + mat.vals[5] * vec->z;
+	vec->z = mat.vals[6] * vec->x + mat.vals[7] * vec->y + mat.vals[8] * vec->z;
+
+	memcpy(vec, &v.x, sizeof(double) * 2);
+}
+
+vec3 mat3x3_axis(mat3x3 me, int i);
+
+vec3 mat3x3_mult_vec(struct mat3x3 mat, struct vec3 vec);
+
+void mat3x3_scale(mat3x3 *mat, double a, double b, double c);
+double mat3x3_length(mat3x3 &mat, int index);
+mat3x3 mat3x3_transpose(mat3x3 &mat);
+double mat3x3_determinant(mat3x3 &mat);
+mat3x3 mat3x3_mult_mat3x3(struct mat3x3 m1, struct mat3x3 m2);
+mat3x3 mat3x3_unit_vec_rotation(vec3 axis, double radians);
+mat3x3 mat3x3_rotate(double alpha, double beta, double gamma);
+mat3x3 mat3x3_ortho_axes(vec3 cVec);
+mat3x3 mat3x3_rhbasis(vec3 aVec, vec3 cVec);
+mat3x3 mat3x3_map_vec_to_vec(vec3 aVec, vec3 bVec);
+mat3x3 mat3x3_closest_rot_mat(vec3 vec1, vec3 vec2, vec3 axis,
+							  double *best = NULL);
+mat3x3 mat3x3_covariance(std::vector<vec3> points);
+
+mat3x3 mat3x3_rot_from_angles(double phi, double psi);
+mat3x3 mat3x3_from_2d_array(double **values);
+void mat3x3_to_2d_array(mat3x3 mat, double ***values);
+void free_2d_array(double **values);
+
+#endif /* defined(__vagabond__mat3x3__) */
---
+++ InEmergencyBreakGlass/vec3.h
@@ -0,0 +1,112 @@
+// Mandexing: a manual indexing program for crystallographic data.
+// Copyright (C) 2017-2018 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__vec3__
+#define __vagabond__vec3__
+
+#include <cstdlib>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+
+struct vec3
+{
+	double x;
+	double y;
+	double z;
+};
+
+struct vec2
+{
+	double x;
+	double y;
+};
+
+double vec3_length(vec3 &vec);
+void vec3_set_length(vec3 *vec, double length);
+
+struct vec3 empty_vec3();
+inline vec3 make_vec3(double x, double y, double z)
+{
+	struct vec3 vec;
+	vec.x = x;
+	vec.y = y;
+	vec.z = z;
+
+	return vec;
+}
+
+vec3 make_randomish_axis();
+
+inline vec2 make_vec2(double x, double y)
+{
+	struct vec2 vec;
+	vec.x = x;
+	vec.y = y;
+
+	return vec;
+}
+
+inline bool vec2_less_vec2(vec2 x, vec2 y)
+{
+	return x.x > y.x;
+}
+
+
+double vec3_sqlength(vec3 &vec);
+struct vec3 vec3_add_vec3(vec3 &aVec, vec3 &bVec);
+struct vec3 vec3_subtract_vec3(vec3 &to, vec3 &from);
+double vec3_dot_vec3(vec3 &aVec, vec3 &bVec);
+double vec3_angle_with_vec3(vec3 &aVec, vec3 &bVec);
+double vec3_cosine_with_vec3(vec3 &aVec, vec3 &bVec);
+vec3 vec3_cross_vec3(vec3 &aVec, vec3 &bVec);
+double vec3_angle_from_three_points(vec3 &aVec, vec3 &bVec, vec3 &cVec);
+double ewald_wavelength(vec3 &aVec);
+
+std::string vec3_desc(vec3 vec);
+
+inline void vec3_min_each(vec3 *minVec, vec3 &aVec)
+{
+	if (aVec.x < minVec->x) minVec->x = aVec.x;
+	if (aVec.y < minVec->y) minVec->y = aVec.y;
+	if (aVec.z < minVec->z) minVec->z = aVec.z;
+}
+
+inline void vec3_max_each(vec3 *maxVec, vec3 &aVec)
+{
+	if (aVec.x > maxVec->x) maxVec->x = aVec.x;
+	if (aVec.y > maxVec->y) maxVec->y = aVec.y;
+	if (aVec.z > maxVec->z) maxVec->z = aVec.z;
+}
+inline void vec3_mult(vec3 *aVec, double mult)
+{
+	aVec->x *= mult;
+	aVec->y *= mult;
+	aVec->z *= mult;
+}
+
+
+inline void vec3_set_length(vec3 *vec, double length)
+{
+	double now = vec3_length(*vec);
+	vec3_mult(vec, length / now);
+}
+
+
+
+#endif /* defined(__vagabond__vec3__) */
